7e30ff9e675ca213e27ed5cdd14b9703abbe4ebd
Duncan Bellamy <dunk@denkimushi.com>
2020-06-06 20:39:06 +0100

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,7 +30,7 @@ fi
 cp -a /home/"$NME"/profile /home/"$NME"/.profile
 [ -n "$OLEFY_BINDPORT" ] && echo "export OLEFY_BINDPORT=$OLEFY_BINDPORT" >> /home/"$NME"/.profile
 [ -n "$OLEFY_TMPDIR" ] && echo "export OLEFY_TMPDIR=$OLEFY_TMPDIR" >> /home/"$NME"/.profile
-[ -n "$OLEFY_LOGLVL=" ] && echo "export OLEFY_LOGLVL=$OLEFY_LOGLVL" >> /home/"$NME"/.profile
+[ -n "$OLEFY_LOGLVL" ] && echo "export OLEFY_LOGLVL=$OLEFY_LOGLVL" >> /home/"$NME"/.profile
 [ -n "$OLEFY_MINLENGTH" ] && echo "export OLEFY_MINLENGTH=$OLEFY_MINLENGTH" >> /home/"$NME"/.profile
 [ -n "$OLEFY_DEL_TMP" ] && echo "export OLEFY_DEL_TMP=$OLEFY_DEL_TMP" >> /home/"$NME"/.profile
 [ -n "$OLEFY_DEL_TMP_FAILED" ] && echo "export OLEFY_DEL_TMP_FAILED=$OLEFY_DEL_TMP_FAILED" >> /home/"$NME"/.profile
